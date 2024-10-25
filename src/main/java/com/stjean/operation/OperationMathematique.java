bd6080889c6394c0190de386f9f0a6b749404519
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-10-25 09:27:53 +0100

    factorial method added

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -5,4 +5,12 @@ public class OperationMathematique {
   public boolean estPositif(int a) {
     return a > 0;
   }
+
+  public int factorial(int n) {
+    if (n == 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
 }
